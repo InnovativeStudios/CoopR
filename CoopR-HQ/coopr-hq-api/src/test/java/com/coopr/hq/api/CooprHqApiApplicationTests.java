bc40ad46899759c50669d7be09f2660e3b0652b3
InnovativeStudios <innovativestudios@outlook.com>
2022-09-11 10:13:56 -0500

    Update 09112022@1015

---
+++ CoopR-HQ/coopr-hq-api/src/test/java/com/coopr/hq/api/CooprHqApiApplicationTests.java
@@ -0,0 +1,10 @@
+package com.coopr.hq.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CooprHqApiApplicationTests {
+    @Test
+    void contextLoads() {}
+}
