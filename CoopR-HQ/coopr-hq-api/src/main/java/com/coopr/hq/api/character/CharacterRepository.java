cbf33e505ed9eb266c90d693d912f1e8a29a73a0
InnovativeStudios <innovativestudios@outlook.com>
2022-09-12 07:07:43 -0500

    Update 09122022@0710

--- CoopR-HQ/coopr-hq-api/src/main/java/com/coopr/hq/api/character/CharacterRepository.java
+++
@@ -1,7 +0,0 @@
-package com.coopr.hq.api.character;
-
-import com.coopr.hq.core.models.Character;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CharacterRepository extends MongoRepository<Character, String> {
-}
--- CoopR-HQ/coopr-hq-api/src/main/java/com/coopr/hq/api/player/PlayerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.coopr.hq.api.player;
-
-import com.coopr.hq.core.models.Player;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface PlayerRepository extends MongoRepository<Player, String> {
-}
